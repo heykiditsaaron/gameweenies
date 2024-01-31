65769ebd604ae7b922c1a490176215827e0e8856
heykiditsaaron <55932658+heykiditsaaron@users.noreply.github.com>
2024-01-31 03:40:27 -0500

    updates

--- components/footer.js
+++ components/footer.js
@@ -1,36 +1,73 @@
+const footerTemplate = document.createElement('template');
+
+footerTemplate.innerHTML = `
+<style>
+.foot {
+  display:  table;
+  background-color: white;
+  border: medium solid black;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px black;
+  width: 85vw;
+  height: fit-content;
+  margin-left: auto;
+  margin-right: auto;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 600;
+}
+.foot a{
+  color: #ff2a2a;
+  text-decoration: none;
+}
+.foot a:hover{
+  color: #ff2a2a;
+  text-decoration: underline;
+}
+.foot p{
+  display: table-row;
+  text-align: center;
+}
+.foot span{
+  display: table-cell;
+  padding: 4px 7px;
+}
+</style>
+  <footer>
+    <div class="foot">
+    <p>
+      <span><a href="index.html"><i class="fa-solid fa-house"></i>Home</a></span>
+      
+      <span><a href="tos.html">Terms</a></span>
+      
+      <span><a href="privacy.html">Privacy Notice</a></span>
+      
+      <span><a href="contact.html">Contact</a></span>
+
+      <span><a href="https://discord.gg"><i class="fab fa-discord"></i></i></a></span>
+
+      <span><a href="https://twitter.com/my-twitter-profile"><i class="fab fa-twitter"></i></a></span>
+      
+      <span><a href="https://www.linkedin.com/in/my-linkedin-profile"><i class="fab fa-linkedin"></i></a></span>
+    </p>
+    </div>
+  </footer>
+`;
+
 class Footer extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
-        <style>
-          ul li {
-            list-style: none;
-            display: inline;
-          }
-          .social-row {
-            font-size: 20px;
-          }
-          
-          .social-row li a {
-            margin: 0 15px;
-          }
-        </style>
-        <footer>
-          <ul>
-            <li><a href="index.html">Home</a></li>
-            <li>|</li>
-            <li><a href="tos.html">Terms and Conditions</a></li>
-            <li>|</li>
-            <li><a href="privacy.html">Privacy Notice</a></li>
-            <li>|</li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-        </footer>
-      `;
-    }
+  constructor() {
+    super();
   }
-  
-  customElements.define('footer-component', Footer);
+
+  connectedCallback() {
+    const fontAwesome = document.querySelector('link[href*="font-awesome"]');
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    if (fontAwesome) {
+      shadowRoot.appendChild(fontAwesome.cloneNode());
+    }
+
+    shadowRoot.appendChild(footerTemplate.content);
+  }
+}
+
+customElements.define('footer-component', Footer);
---
+++ components/header.js
@@ -0,0 +1,62 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+  <style>
+    nav {
+      width: 85vw;
+      height: 40px;
+      margin-left: auto;
+      margin-right: auto;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color:  #0a0a23;
+      border-radius: 10px;
+      box-shadow: 2px 2px 5px black;
+    }
+
+    ul {
+      padding: 0;
+    }
+    
+    ul li {
+      list-style: none;
+      display: inline;
+    }
+    
+    a {
+      font-weight: 700;
+      margin: 0 25px;
+      color: #fff;
+      text-decoration: none;
+    }
+    
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #fff;
+    }
+  </style>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="about.html">About</a></li>
+        <li><a href="work.html">Work</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+`;
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    shadowRoot.appendChild(headerTemplate.content);
+  }
+}
+
+customElements.define('header-component', Header);
